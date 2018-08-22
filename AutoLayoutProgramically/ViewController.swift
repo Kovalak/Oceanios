aa6890dd1e82fd3bbd2114a57e4b842699c0b468
ehsan kolivand <ehsankolivand@ehsans-macbook-pro.local>
2018-08-22 18:59:25 +0430

    Initial Commit

---
+++ AutoLayoutProgramically/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayoutProgramically
+//
+//  Created by ehsan kolivand on 8/22/18.
+//  Copyright © 2018 ehsan kolivand. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
